55184fbe8f130390037c0bbabc4c3aa9269562dd
shi4ts <56309809+shi4ts@users.noreply.github.com>
2019-11-15 21:21:59 +0800

    Update 4_tree.pl

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -12,9 +12,6 @@ insert(X,[Y|T],[Y|T1]):-X>Y,insert(X,T,T1).
 insert(X,[Y|T],[X,Y|T]):-X=<Y.
 insert(X,[],[X]).
 
-balanced_tree([],[]):- write('empty').
-balanced_tree(L,T) :- inl(L,empty,T).
-
 inl([N|Ns], T0, T) :-
     instant(N, T0, T1),
     inl(Ns, T1, T).
@@ -30,9 +27,12 @@ instant(I, t(X, L, R), t(V, B, N)) :-
         (V, B, N) = (L, X, U)
     ;   (V, B, N) = (L, X, R)
     ).
+    
+balanced_tree([],[]):- write('empty').
+balanced_tree(L,T) :- inl(L,empty,T).
+
 
 ?- balanced_tree([3,4,3,5,4],S).
 S = t(3, empty, t(4, empty, t(5, empty, empty))) 
 
-?- balanced_tree([3,4,3,5,4],S).
-S = t(3, empty, t(4, empty, t(5, empty, empty))) 
+

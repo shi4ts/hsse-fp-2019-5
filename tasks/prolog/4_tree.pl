d8565b7cfc6aaf5b2d0637c3b53532565e132f0e
shi4ts <56309809+shi4ts@users.noreply.github.com>
2019-11-15 21:20:02 +0800

    Update 4_tree.pl

--- tasks/prolog/4_tree.pl
+++ tasks/prolog/4_tree.pl
@@ -3,3 +3,35 @@
 % для построения дерева использовать следующие  нотации:
 % empty - пустое дерево 
 % instant(R, L, R) - бинарное дерево с корнем R и двумя поддеревьями L и R соотвественно (левое и правое)
+
+
+
+qsort(L,K):-isort(L,[],K).
+isort([],As,As).
+isort([H|T],As,R):-insert(H,As,As1),isort(T,As1,R).
+
+insert(X,[Y|T],[Y|T1]):-X>Y,insert(X,T,T1).
+insert(X,[Y|T],[X,Y|T]):-X=<Y.
+insert(X,[],[X]).
+
+balanced_tree([],[]):- write('empty').
+balanced_tree(L,T) :- inl(L,empty,T).
+
+inl([N|Ns], T0, T) :-
+    instant(N, T0, T1),
+    inl(Ns, T1, T).
+inl([], T, T).
+
+instant(I, empty, t(I, empty, empty)).
+instant(I, t(X, L, R), t(V, B, N)) :-
+    (   I < X
+    ->  instant(I, L, U),
+        (V, B, N) = (U, X, R)
+    ;   I > X
+    ->  instant(I, R, U),
+        (V, B, N) = (L, X, U)
+    ;   (V, B, N) = (L, X, R)
+    ).
+
+?- balanced_tree([3,4,3,5,4],S).
+S = t(3, empty, t(4, empty, t(5, empty, empty))) 
